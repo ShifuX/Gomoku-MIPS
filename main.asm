a8e6fd6edad2c62743c6f157c8ea2da937a4dc0c
ShifuX <91515578+ShifuX@users.noreply.github.com>
2022-10-31 17:19:57 -0500

    Board now displays with numbers
    Added some space for styling purposes btwn the numbers. Some issue where the first row is printing 18 dots instead of 19 (WEIRD).
    Need to add the letters at the top and at the bottom of the board to finish.

--- main.asm
+++ main.asm
@@ -1,10 +1,12 @@
 .data
 board: .space 361
 blank_piece: .ascii "."
+space: .asciiz " "
 nl: .ascii "\n"
 sub_counter: .word 1
 sub_counter2: .word 18
-test: .asciiz "INNNN"
+boardNum: .word 1
+
 .text
 main:
 	addi $t0, $zero, 0
@@ -13,7 +15,6 @@ main:
 	jal fill_board
 	addi $t8, $zero, 1
 	jal display_board
-	
 	
 	li $v0, 10
 	syscall
@@ -32,13 +33,12 @@ exit_fill:
 display_board:	# Displays the board through a loop
 	beq $t0, 361, exit_board
 	
-	beq $t0, $t8, add_num_to_board
-	add_num_return:
+	beq $t0, 1, first_board_num
+	first_board_return:
 	
 	lb $t2, board($t0)
-	
 	addi $t3, $zero, 19
-	div $t0, $t3
+	div $t0, $t3	# $t0 % 19
 	mfhi $t4	# Gets the remainder
 	
 	beq $t4, 0, add_nl
@@ -57,50 +57,80 @@ exit_board:
 	addi $t5, $zero, 18
 	sw $t5, sub_counter2
 	
+	li $v0, 11
+	lb $a0, space
+	syscall
+	
 	li $v0, 1
 	la $a0, ($t6)
 	syscall
 	
-	addi $t0, $zero, 0
+	addi $t0, $zero, 0	# Resetting registers
 	addi $t2, $zero, 0
 	jr $ra
-
-add_num_to_board:
-	lw $t5, sub_counter
-	sub $t6, $t0, $t5
-	sub $t7, $t0, $t6
-	addi $t5, $t5, 1
-	sw $t5, sub_counter
-	
-	li $v0, 1
-	la $a0, ($t7)
-	syscall
-	
-	addi $t5, $zero, 0
-	addi $t6, $zero, 0
-	addi $t7, $zero, 0
-	addi $t8, $t8, 19
-	j add_num_return
 			
 add_nl:	# Adds a new line to create a row
 	bgtz $t0, add_num_to_board2
 	add2_return:
+	lw $t5, boardNum
+
 	li $v0, 4
 	la $a0, nl
 	syscall
+	
+	li $v0, 1
+	la $a0, ($t5)
+	syscall
+	
+	li $v0, 11
+	lb $a0, space
+	syscall
+	
+	ble $t5, 9, extra_space
+	extra_space_return:
+	
+	addi $t5, $zero, 0
+	addi $t8, $t8, 18	# Increment counter
 	j nl_return
 
+first_board_num:	# Prints the first right num
+	li $v0, 1
+	addi $a0, $zero, 1
+	syscall
+	
+	li $v0, 11
+	lb $a0, space
+	syscall
+	
+	li $v0, 11
+	lb $a0, space
+	syscall
+	
+	j first_board_return
+	
 add_num_to_board2: # Displays the number on the right side of the board
-	lw $t5, sub_counter2
+	lw $t5, sub_counter2	# ($t0 - ($t0 - i)) where i increments after each call
 	sub $t6, $t0, $t5
 	addi $t5, $t5, 18
 	sw $t5, sub_counter2
+	
+	li $v0, 11
+	lb $a0, space
+	syscall
 	
 	li $v0, 1
 	la $a0, ($t6)
 	syscall
 	
-	addi $t5, $zero, 0
+	addi $t6, $t6, 1
+	sw $t6, boardNum
+	
+	addi $t5, $zero, 0	# Reset registers
 	addi $t6, $zero, 0
 	j add2_return
-	
+
+extra_space:
+	li $v0, 11
+	lb $a0, space
+	syscall
+	j extra_space_return
